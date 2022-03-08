9a7130eef6d66310943b9b9db10ab99f1019cad2
mnmsby <91547882+mnmsby@users.noreply.github.com>
2022-03-08 12:17:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,8 +29,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 default_start_msg = """
-**Hi, I'm Media Search Bot or ypu can call me as Auto-Filter Bot**
-Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
+**Hi, I'm Graphics File Search Bot or you can call me as Auto-Filter Bot 😉**
+Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name or id of files to search.
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
 
