294eaa04bae4e678855cd22a89ce65cb7425ef8a
mnmsby <91547882+mnmsby@users.noreply.github.com>
2022-03-08 18:12:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from info import START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 from utils import Media, get_file_details, get_size
 from pyrogram.errors import UserNotParticipant
