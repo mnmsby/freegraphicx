4cc9c51b70fd4e96a1014774770a3eaff29bf4f5
mnmsby <91547882+mnmsby@users.noreply.github.com>
2022-03-14 14:32:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,6 +2,7 @@ import os
 import logging
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from logo import logo-pencemo-2.jpg
 from info import START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, DATABASE_URI
 from utils import Media, get_file_details, get_size
 from pyrogram.errors import UserNotParticipant
